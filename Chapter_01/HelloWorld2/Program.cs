95d609a65ca01ce5060d20e3a5869df1d102c0ae
Tri Phan <phanphutri@gmail.com>
2022-03-12 15:37:27 +0700

    Updated

---
+++ Chapter_01/HelloWorld2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace HelloWorld2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aloha World!");
+        }
+    }
+}
