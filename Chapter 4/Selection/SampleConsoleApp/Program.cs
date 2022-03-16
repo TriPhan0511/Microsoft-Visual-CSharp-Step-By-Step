fb88e9c3a550af093336728120c5061e3ca47967
Tri Phan <phanphutri@gmail.com>
2022-03-16 20:36:04 +0700

    Updated

---
+++ Chapter 4/Selection/SampleConsoleApp/Program.cs
@@ -0,0 +1,174 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+namespace SampleConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            // int type
+            //int day = ReadDay("Enter a day: ");
+            //DisplayADay(day);
+
+            // string type
+            //string day = ReadDay2("Enter a day: ");
+            //DisplayADay2(day);
+
+            // char type
+            char day = ReadDay3("Enter a day: ");
+            DisplayADay3(day);
+        }
+
+        // Helper method
+        private void DisplayADay3(char day)
+        {
+            string dayName;
+            switch (day)
+            {
+                case '0':
+                    dayName = "Sunday";
+                    break;
+                case '1':
+                    dayName = "Monday";
+                    break;
+                case '2':
+                    dayName = "Tuesday";
+                    break;
+                case '3':
+                    dayName = "Wednesday";
+                    break;
+                case '4':
+                    dayName = "Thursday";
+                    break;
+                case '5':
+                    dayName = "Friday";
+                    break;
+                case '6':
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Unknown";
+                    break;
+            }
+            Console.WriteLine(dayName);
+        }
+
+        // Helper method
+        private char ReadDay3(string prompt)
+        {
+            Console.Write(prompt);
+            return (char)Console.Read();
+        }
+
+        // Helper method
+        private void DisplayADay2(string day)
+        {
+            string dayName;
+            switch (day)
+            {
+                case "0":
+                    dayName = "Sunday";
+                    break;
+                case "1":
+                    dayName = "Monday";
+                    break;
+                case "2":
+                    dayName = "Tuesday";
+                    break;
+                case "3":
+                    dayName = "Wednesday";
+                    break;
+                case "4":
+                    dayName = "Thursday";
+                    break;
+                case "5":
+                    dayName = "Friday";
+                    break;
+                case "6":
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Unknown";
+                    break;
+            }
+            Console.WriteLine(dayName);
+        }
+
+        // Helper method
+        private string ReadDay2(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string day = Console.ReadLine();
+                if (day != null && !day.Equals(""))
+                {
+                    return day;
+                }
+                else
+                {
+                    Console.WriteLine("You should enter a valid day (0-6).");
+                }
+            }
+        }
+
+        // Helper method
+        private static void DisplayADay(int day)
+        {
+            string dayName;
+            switch (day)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Unknown.";
+                    break;
+            }
+            Console.WriteLine(dayName);
+        }
+
+        // Helper method
+        private int ReadDay(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                    return int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong input. Please enter an integer.");
+                }
+            }
+
+        }
+    }
+}
---
+++ Chapter 4/SwitchStatement/SwitchStatementConsoleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+namespace SwitchStatementConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int measurement = 100;
+            string range = measurement switch
+            {
+                < 0 => "negative",
+                0 => "zero",
+                >= 1 and <= 9 => "singledigit",
+                >= 10 and <= 99 => "doubledigit",
+                >= 100 => "large"
+            };
+            Console.WriteLine(range);
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    string input = "inRange = (lo <= number) && (hi >= number) \" '";
+        //    string output = ConvertIntoXML(input);
+        //    Console.WriteLine(input);
+        //    Console.WriteLine(output);
+        //}
+
+        private static string ConvertIntoXML(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                char item = input[i];
+                switch (item)
+                {
+                    case '>':
+                        output += "&gt;";
+                        break;
+                    case '<':
+                        output += "&lt;";
+                        break;
+                    case '&':
+                        output += "&amp;";
+                        break;
+                    case '"':
+                        output += "&#34;";
+                        break;
+                    case '\'':
+                        output += "&#39;";
+                        break;
+                    default:
+                        output += item;
+                        break;
+                }
+            }
+            return output; 
+        }
+
+        private static string ReadInput(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                if (input == null || input.Length  == 0)
+                {
+                    Console.WriteLine("You should not enter an empty string.\n" +
+                        "Please enter a string again.");
+                }
+                else
+                {
+                    return input;
+                }
+            }
+        }
+    }
+}
+
