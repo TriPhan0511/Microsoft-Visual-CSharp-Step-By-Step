9914f47ca4ccc607868965e72e46bd5f581e07e6
Tri Phan <phanphutri@gmail.com>
2022-05-02 11:19:38 +0700

    Updated

---
+++ CSharp_101_dotNet/MyBank/MyBank/Classes/Transaction.cs
@@ -0,0 +1,15 @@
+﻿namespace Classes;
+public class Transaction
+{
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Notes { get; set; }
+
+    public Transaction(decimal amount, DateTime date, string note)
+    {
+        Amount = amount;
+        Date = date;
+        Notes = note;
+    }
+}
+
--- CSharp_101_dotNet/MyBank/MyBank/Program.cs
+++ CSharp_101_dotNet/MyBank/MyBank/Program.cs
@@ -1,9 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using MyBank;
+using Classes;
 
-var account = new BankAccount("Alex", 1000);
-account.MakeWithdrawal(200, DateTime.Now, "Iphone 4");
-account.MakeDeposit(550, DateTime.Now, "Friend pay back");
-Console.WriteLine(account.GetTransactionLog());
+//var account = new BankAccount("Alex", 1000);
+//account.MakeWithdrawal(200, DateTime.Now, "Iphone 4");
+//account.MakeDeposit(550, DateTime.Now, "Friend pay back");
+//Console.WriteLine(account.GetAccountHistory());
 
+var account = new BankAccount("Alex Ferguson", 1000);
+Console.WriteLine($"{account.Number} is created for {account.Onwer} with {account.Balance} initial balance.");
+
+var account2 = new InterestEarningAccount("Peter", 500);
+Console.WriteLine($"{account2.Number} is created for {account2.Onwer} with {account2.Balance} initial balance.");
+
+Console.WriteLine(account.GetType());
+//if (account.GetType() == typeof(BankAccount))
+//if (account2.GetType() == typeof(BankAccount))
+if (account2.GetType() == typeof(InterestEarningAccount))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
 
